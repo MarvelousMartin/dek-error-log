ca321fa2eb0f0a8b3d570f02c3ff5ab30e2aa6e9
Dub Martin <martin.dub@dek-cz.com>
2022-06-30 13:46:30 +0200

    Database sandbox

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-const btn = document.getElementsByClassName('btn btn-error')[0];
+function toggle(el) {
+    let tag = document.getElementById(el);
+    tag.style.display = tag.style.display === 'block' ? 'none' : 'block';
+}
 
+
+const btn = document.getElementsByClassName('btn btn-error')[0];
+const div = document.getElementById('showhide');
 let index = 0;
 
 const colors = ['darkgreen','red', 'gray','#0d6efd'];
 
 btn.addEventListener('click', function onClick() {
-    btn.style.backgroundColor = colors[index];
-    btn.style.color = 'white';
-    index++;
-    if (index >= colors.length) {
-        index = 0;
+    switch (index) {
+        case 0:
+            btn.style.backgroundColor = 'darkgreen';
+            btn.innerText = 'Přiřazen na úkolu';
+            index++;
+            break;
+        case 1:
+            btn.style.backgroundColor = 'red';
+            btn.innerText = 'Odhlásit se z úkolu?';
+            index++;
+            break;
+        case 2:
+            btn.style.backgroundColor = 'gray';
+            btn.innerText = 'Odhlášen z úkolu';
+            index++;
+            break;
+        case 3:
+            btn.style.backgroundColor = '#0d6efd';
+            btn.innerText = 'Přiřadit se na úkol';
+            index = 0;
+            break;
     }
 });

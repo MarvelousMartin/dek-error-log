806cc56989a30dac0528e66c2e956298ac998b45
Dub Martin <martin.dub@dek-cz.com>
2022-06-30 14:40:05 +0200

    Bold text @ No. of occurrences

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                 <li><a href="#">Ipsum</a></li>
             </ul>
             -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-bug-fill" viewBox="0 0 16 16" opacity="0.1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-bug-fill" viewBox="0 0 16 16" opacity="0.05">
                 <path d="M4.978.855a.5.5 0 1 0-.956.29l.41 1.352A4.985 4.985 0 0 0 3 6h10a4.985 4.985 0 0 0-1.432-3.503l.41-1.352a.5.5 0 1 0-.956-.29l-.291.956A4.978 4.978 0 0 0 8 1a4.979 4.979 0 0 0-2.731.811l-.29-.956z"/>
                 <path d="M13 6v1H8.5v8.975A5 5 0 0 0 13 11h.5a.5.5 0 0 1 .5.5v.5a.5.5 0 1 0 1 0v-.5a1.5 1.5 0 0 0-1.5-1.5H13V9h1.5a.5.5 0 0 0 0-1H13V7h.5A1.5 1.5 0 0 0 15 5.5V5a.5.5 0 0 0-1 0v.5a.5.5 0 0 1-.5.5H13zm-5.5 9.975V7H3V6h-.5a.5.5 0 0 1-.5-.5V5a.5.5 0 0 0-1 0v.5A1.5 1.5 0 0 0 2.5 7H3v1H1.5a.5.5 0 0 0 0 1H3v1h-.5A1.5 1.5 0 0 0 1 11.5v.5a.5.5 0 1 0 1 0v-.5a.5.5 0 0 1 .5-.5H3a5 5 0 0 0 4.5 4.975z"/>
             </svg>
@@ -39,7 +39,7 @@
         <div class="row">
             <div>
                 <h1 style="display: inline-block">Log</h1>
-                <a id="dropdownMenuLink" class="btn align-self-center" href="#" style="float: right" onclick="toggle('el')">
+                <a id="dropdownMenuLink" class="btn align-self-center" href="#" style="float: right">
                     <svg style="float: right" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="lightgrey" class="bi bi-funnel-fill" viewBox="0 0 16 16">
                         <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2z"/>
                     </svg>
@@ -86,7 +86,7 @@
 
                         if ($result->num_rows > 0) {
                             $counter = 1;
-                            echo "Nalezeno " . $result->num_rows . " záznamů";
+                            echo "Nalezeno " . '<strong>'.$result->num_rows .'</strong>'." záznamů";
                             while($row = $result->fetch_assoc()) {
                                 echo "<tr>";
                                 echo '<td>' . $row["id"] . "</td>";

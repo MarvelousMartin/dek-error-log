0fc009f453e107967eb2ab5827728d16394ac4c4
Dub Martin <martin.dub@dek-cz.com>
2022-06-30 08:29:39 +0200

    Date show modified

--- index.php
+++ index.php
@@ -63,12 +63,12 @@
                         </thead>
                         <tbody>
                         <?php
-                        $i = 100;
+                        $i = 1;
                         function getErrors(): array
                         {
                             $errors = array();
                             $errors[] = array(
-                                'date' => date('d.m.Y H:i:s'),
+                                'date' => date('d.m.Y'),
                                 'error' => '404',
                                 'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                             );
